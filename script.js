b3c800f20edd03949cb42ab880a016cdca8fae98
Alisya Saffana <92367305+alisyasaffana@users.noreply.github.com>
2021-11-15 01:38:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 script.js
+const numbers = document.querySelectorAll(".number")
+console.log(numbers)
+
+const numbers = document.querySelectorAll(".number")
+numbers.forEach( (number) => {
+  number.addEventListener("click", () => {
+console.log("number is pressed")
+})
+})
+
+const numbers = document.querySelectorAll(".number")
+numbers.forEach( (number) => {
+  number.addEventListener("click", (event) => {
+console.log(event.target.value)
+})
+})
+
+const calculatorScreen = document.querySelector('.calculator-screen')
+
+const updateScreen = (number) => {
+  calculatorScreen.value = number
+}
+
+const numbers = document.querySelectorAll(".number")
+numbers.forEach( (number) => {
+  number.addEventListener("click", (event) => {
+console.log(event.target.value)
+})
+})
+
